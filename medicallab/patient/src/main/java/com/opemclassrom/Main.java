e101a2504317ff732009ff0d317f731187c12742
bpbanga <97730466+bpbanga@users.noreply.github.com>
2024-05-15 21:20:54 +0200

    Initial commit

---
+++ medicallab/patient/src/main/java/com/opemclassrom/Main.java
@@ -0,0 +1,7 @@
+package com.opemclassrom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ medicallab/src/main/java/com/opemclassrom/medicallab/MedicallabApplication.java
@@ -0,0 +1,13 @@
+package com.opemclassrom.medicallab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicallabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicallabApplication.class, args);
+	}
+
+}
